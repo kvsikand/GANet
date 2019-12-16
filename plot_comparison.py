1291a2aca154809265ebd0d265b5926fa8210a4b
Kavan Sikand <kvsikand@gmail.com>
2019-12-16 16:05:15 -0600

    handle nonpng files

--- plot_comparison.py
+++ plot_comparison.py
@@ -20,14 +20,15 @@ print(fl2)
 f, axarr = plt.subplots(sz, 3, figsize=(15, 5))
 
 for f2 in fl2: 
-    img1 = mpimg.imread(os.path.join(compdir, f2[:-4] + '_input.png'))
-    img2 = mpimg.imread(os.path.join(refdir, f2))
-    img3 = mpimg.imread(os.path.join(compdir, f2))
+    if '.png' in f2:
+        img1 = mpimg.imread(os.path.join(compdir, f2[:-4] + '_input.png'))
+        img2 = mpimg.imread(os.path.join(refdir, f2))
+        img3 = mpimg.imread(os.path.join(compdir, f2))
 
-    axarr[i,0].imshow(img1)
-    axarr[i,1].imshow(img2)
-    axarr[i,2].imshow(img3)
-    i += 1
-    if i == sz:
-        break
+        axarr[i,0].imshow(img1)
+        axarr[i,1].imshow(img2)
+        axarr[i,2].imshow(img3)
+        i += 1
+        if i == sz:
+            break
 f.savefig("result.png", dpi=f.dpi)
