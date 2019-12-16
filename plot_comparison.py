3a264ef2048ec37a2a488591cea4941c27ae9f13
Kavan Sikand <kvsikand@gmail.com>
2019-12-16 16:07:11 -0600

    better handling

--- plot_comparison.py
+++ plot_comparison.py
@@ -13,6 +13,8 @@ compdir = opt.comp_dir
 
 fl1 = sorted(os.listdir(refdir))
 fl2 = sorted(os.listdir(compdir))
+
+fl2 = list(filter(lambda x: '.png' in x and '_input' not in x. fl2))
 i = 0
 sz = len(fl2)
 print(fl2)
@@ -20,15 +22,14 @@ print(fl2)
 f, axarr = plt.subplots(sz, 3, figsize=(15, 5))
 
 for f2 in fl2: 
-    if '.png' in f2 and '_input' not in f2:
-        img1 = mpimg.imread(os.path.join(compdir, f2[:-4] + '_input.png'))
-        img2 = mpimg.imread(os.path.join(refdir, f2))
-        img3 = mpimg.imread(os.path.join(compdir, f2))
+    img1 = mpimg.imread(os.path.join(compdir, f2[:-4] + '_input.png'))
+    img2 = mpimg.imread(os.path.join(refdir, f2))
+    img3 = mpimg.imread(os.path.join(compdir, f2))
 
-        axarr[i,0].imshow(img1)
-        axarr[i,1].imshow(img2)
-        axarr[i,2].imshow(img3)
-        i += 1
-        if i == sz:
-            break
+    axarr[i,0].imshow(img1)
+    axarr[i,1].imshow(img2)
+    axarr[i,2].imshow(img3)
+    i += 1
+    if i == sz:
+        break
 f.savefig("result.png", dpi=f.dpi)
